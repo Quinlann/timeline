49f774f6514ba383f15f6f5e91dca06a0a53ae4c
Quinlann <hegs_hta@hotmail.com>
2023-03-28 21:15:56 +0200

    Added VueJS to the project!

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default {
+  components: {ChildComponent},
+  data() {
+    return {
+      propForChild: 'Hello from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComponent :parentProp="propForChild" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: ['parentProp'],
+  data() {
+    return {
+      myProp: 'Hello from child!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ parentProp }}</h1>
+  {{  myProp }}
+</template>
+
+<style scoped>
+
+</style>
