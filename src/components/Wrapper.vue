3859697b2e8a42eccdbb7cd265686b4922a93d94
Bem5 <suc@live.dk>
2023-04-15 19:54:10 +0200

    Added Creation window

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -39,7 +39,42 @@ document.addEventListener("wheel", function(e) {
 
 <template>
 <div id="wrapper">
-    <div class="map">
+    <div class="creation">
+		<div class="creation__header">
+			<div class="top">
+				<div class="icon">O</div>
+				<div class="name">[Name]</div>
+				<div class="closeBtn">X</div>
+			</div>
+			<div class="bottom">
+				<div class="path">World 1</div>
+			</div>
+		</div>
+		<div class="creation__info">
+			<div class="section">
+				<div class="section__title">Nickname</div>
+				<div class="section__content"><input type="text"></div>
+			</div>
+			<div class="section">
+				<div class="section__title">Description</div>
+				<div class="section__content"><input type="text"></div>
+			</div>
+			<div class="section">
+				<div class="section__title">Images</div>
+				<div class="section__content">
+					<div class="btn add"><font-awesome-icon icon="fa-solid fa-plus" /></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="creation__timeline">
+			<div class="section">
+				<div class="section__title">Timeline</div>
+				<div class="section__content"></div>
+			</div>
+		</div>
+	</div>
+	<div class="map">
 		<img src="/src/assets/worldmap.svg">
 		<Entity v-for="entity in this.entities" :color="entity.color" :x="entity.x" :y="entity.y" :name="entity.text"/>
 	</div>
@@ -52,17 +87,104 @@ document.addEventListener("wheel", function(e) {
 @icon-size: 2rem;
 
 #wrapper {
-	//background-color: @green;
 	min-height: calc(100vh - @navigation-height);
 	position: relative;
 	padding-top: @navigation-height;
 	display: flex;
-	justify-items: center;
+	justify-content: center;
 	align-items: center;
 }
 
+.creation {
+	background-color: lightgrey;
+	max-width: 45rem;
+	width: 45rem;
+	border-radius: .5rem;
+	padding: .5rem;
+	box-shadow: 0 5px 10px fade(black, 20%);
+	z-index: 1;
+
+	&__header {
+		display: flex;
+		flex-direction: column;
+		border-bottom: 1px solid @black;
+		padding-bottom: .5rem;
+		margin-bottom: .5rem;
+		.top { margin-bottom: .25rem }
+		
+		.icon {
+			margin-right: .25rem;
+			float: left;
+		}
+		
+		.name {
+			text-transform: uppercase;
+			font-weight: bold;
+			float: left;
+		}
+
+		.closeBtn {
+			float: right;
+		}
+
+		.path {
+			font-size: .8rem;
+		}
+	}
+
+	&__info {
+		float: left;
+		width: ~"calc(100% - 15.5rem)";
+		margin-right: .5rem;
+	}
+
+	&__timeline {
+		float: left;
+		width: 15rem;
+	}
+
+	.section {
+		&:not(:last-child) { margin-bottom: .5rem }
+		
+		&__title {
+			text-transform: uppercase;
+			font-weight: bold;
+			margin: 0 0 .25rem .25rem;
+		}
+
+		&__content {
+			padding: .5rem;
+			background-color: fade(@white, 20%);
+			border-radius: .5rem;
+		}
+
+		.btn {
+			min-width: 4rem;
+			width: 4rem;
+			height: 5rem;
+			background-color: fade(@black, 10%);
+			
+			&.add {
+				text-align: center;
+				line-height: 5rem;
+				border-radius: .5rem;
+			}
+
+			&:hover {
+				background-color: fade(@black, 20%);
+			}
+		}
+
+		input {
+			width: 100%;
+			border: none;
+			padding: .25rem 0;
+			background-color: fade(white, 50%);
+		}
+	}
+}
+
 .map {
-	//background: url('/src/assets/worldmap.svg') no-repeat center;
 	background-size: contain;
 	width: 100%;
 	position: fixed;
