7c6d09e65514350ada66716e6a83af82f97b4603
Quinlann <hegs_hta@hotmail.com>
2023-05-17 23:41:38 +0200

    Added drag

--- src/components/wrapper/Creation.vue
+++ src/components/wrapper/Creation.vue
@@ -162,6 +162,44 @@ export default {
 			if(title === 'Categories') this.showingCategories = false;
 			if(title === 'Teams') this.showingTeams = false;
 		},
+		createDrag() {
+			// https://www.w3schools.com/howto/howto_js_draggable.asp
+			const popupNode = document.querySelector('.creation');
+			dragElement(popupNode);
+
+			function dragElement(elmnt) {
+				var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+				popupNode.querySelector('.creation__header').onmousedown = dragMouseDown;
+
+				function dragMouseDown(e) {
+					e = e || window.event;
+					e.preventDefault();
+					pos3 = e.clientX;
+					pos4 = e.clientY;
+					document.onmouseup = closeDragElement;
+					document.onmousemove = elementDrag;
+				}
+
+				function elementDrag(e) {
+					e = e || window.event;
+					e.preventDefault();
+					pos1 = pos3 - e.clientX;
+					pos2 = pos4 - e.clientY;
+					pos3 = e.clientX;
+					pos4 = e.clientY;
+					elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+					elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+				}
+
+				function closeDragElement() {
+					document.onmouseup = null;
+					document.onmousemove = null;
+				}
+			}
+		},
+	},
+	mounted(){
+		this.createDrag();
 	},
 }
 </script>
@@ -175,6 +213,7 @@ export default {
 @margin: .5rem;
 
 .creation {
+	position: absolute;
 	background-color: fade(black, 20%);
 	backdrop-filter: blur(10px);
 	max-width: 45rem;
